46e04e2e213f8cca6458914d5aad675c12a6d22a
Muhammad Suleman <msulaman061@gmail.com>
2024-06-20 17:37:36 +0500

    bottom tab bar created

--- src/routes/BottomTabs.js
+++ src/routes/BottomTabs.js
@@ -1,9 +1,23 @@
+import {View, Text, StyleSheet} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import {SkipItColors} from '../constants/SkipItColors';
+import {SkipItFonts} from '../constants/SkipItFonts';
 
 import ProductList from '../screens/ProductList';
 import NewsScreen from '../components/screensUi/NewsScreen';
 import ScannerScreen from '../components/screensUi/ScannerScreen';
 import AboutScreen from '../components/screensUi/AboutScreen';
+
+import ListIcon from 'react-native-vector-icons/Feather';
+//list
+import NewsIcon from 'react-native-vector-icons/Ionicons';
+//newspaper-outline
+import ScanIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+//barcode-scan
+import AboutIcon from 'react-native-vector-icons/Ionicons';
+
+//
 
 export const MyBottomTabs = () => {
   const Tab = createBottomTabNavigator();
@@ -12,11 +26,158 @@ export const MyBottomTabs = () => {
       initialRouteName="NewsScreen"
       screenOptions={{
         headerShown: false,
+        tabBarStyle: {
+          height: 70,
+          paddingBottom: 5,
+          paddingTop: 5,
+          backgroundColor: SkipItColors.PrimaryGreen,
+        },
       }}>
-      <Tab.Screen name="ProductList" component={ProductList} />
-      <Tab.Screen name="NewsScreen" component={NewsScreen} />
-      <Tab.Screen name="ScannerScreen" component={ScannerScreen} />
-      <Tab.Screen name="AboutScreen" component={AboutScreen} />
+      <Tab.Screen
+        name="ProductList"
+        component={ProductList}
+        options={{
+          tabBarLabel: ({focused}) => {
+            return (
+              <View>
+                <Text
+                  style={[
+                    styles.tabBarLabelText,
+                    {
+                      color: focused
+                        ? SkipItColors.FocusRed
+                        : SkipItColors.BachgroundWhite,
+                    },
+                  ]}>
+                  List
+                </Text>
+              </View>
+            );
+          },
+          tabBarIcon: ({focused}) => {
+            return (
+              <ListIcon
+                name="list"
+                size={30}
+                color={
+                  focused ? SkipItColors.FocusRed : SkipItColors.BachgroundWhite
+                }
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="NewsScreen"
+        component={NewsScreen}
+        options={{
+          tabBarLabel: ({focused}) => {
+            return (
+              <View>
+                <Text
+                  style={[
+                    styles.tabBarLabelText,
+                    {
+                      color: focused
+                        ? SkipItColors.FocusRed
+                        : SkipItColors.BachgroundWhite,
+                    },
+                  ]}>
+                  News
+                </Text>
+              </View>
+            );
+          },
+          tabBarIcon: ({focused}) => {
+            return (
+              <NewsIcon
+                name="newspaper-outline"
+                size={30}
+                color={
+                  focused ? SkipItColors.FocusRed : SkipItColors.BachgroundWhite
+                }
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="ScannerScreen"
+        component={ScannerScreen}
+        options={{
+          tabBarLabel: ({focused}) => {
+            return (
+              <View>
+                <Text
+                  style={[
+                    styles.tabBarLabelText,
+                    {
+                      color: focused
+                        ? SkipItColors.FocusRed
+                        : SkipItColors.BachgroundWhite,
+                    },
+                  ]}>
+                  Scan
+                </Text>
+              </View>
+            );
+          },
+          tabBarIcon: ({focused}) => {
+            return (
+              <ScanIcon
+                name="barcode-scan"
+                size={30}
+                color={
+                  focused ? SkipItColors.FocusRed : SkipItColors.BachgroundWhite
+                }
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="AboutScreen"
+        component={AboutScreen}
+        options={{
+          tabBarLabel: ({focused}) => {
+            return (
+              <View>
+                <Text
+                  style={[
+                    styles.tabBarLabelText,
+                    {
+                      color: focused
+                        ? SkipItColors.FocusRed
+                        : SkipItColors.BachgroundWhite,
+                    },
+                  ]}>
+                  About
+                </Text>
+              </View>
+            );
+          },
+          tabBarIcon: ({focused}) => {
+            return (
+              <AboutIcon
+                name="information-circle-outline"
+                size={30}
+                color={
+                  focused ? SkipItColors.FocusRed : SkipItColors.BachgroundWhite
+                }
+              />
+            );
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  tabBarLabelText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontFamily: SkipItFonts.HeadingFont,
+  },
+});
